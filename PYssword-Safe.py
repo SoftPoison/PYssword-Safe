f58081719a7e790f7f83c52faca561686e7bb747
SoftPoison <williamson.tomais@gmail.com>
2016-08-06 20:50:46 +1200

    Added some password verification stuff
    + other stuff

--- PYssword-Safe.py
+++ PYssword-Safe.py
@@ -4,6 +4,8 @@ import sys, os, hashlib, json, re, random, base64
 
 #File contents: {"user@email.com": ("hashed master password", "salt", "encrypted text")}
 
+password_check = re.compile("^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8,56}$")
+
 def cls(): os.system("cls")
 
 def generate_salt(length=24):
@@ -15,33 +17,39 @@ class Safe:
         self.info = "Welcome to PYssword Safe"
         self.current_user = ""
         self.username = ""
-        self._raw_data = {}
-        self.cur_data = {}
+        self.raw_data = {}
         self.cipher = None
         
         try:
             with open("safe.lck", "r") as f:
-                self._raw_data = json.dumps(f.read())
+                self.raw_data = json.loads(f.read())
                 f.close()
         
-        except: self._create_account()
+        except: self.create_account()
 
-        self.accounts = self._raw_data.keys()
+        self.accounts = self.raw_data.keys()
+        self.main()
 
-    def _exit(self):
+    def exit(self):
+        if not self.current_user == "":
+            encrypted = self.cipher.encrypt(json.dumps(self.cur_data).encode("utf-8"))
+            self.raw_data[self.current_user][3] = encrypted.decode("utf-8")
+
+            os.system("@echo off & del safe.lck")
+
+            os.system("echo {} > safe.lck".format(json.dumps(self.raw_data)))
+        
         cls()
         print("Exiting...")
-        if not self.current_user == "":
-            pass
         
         sys.exit()
 
-    def _input(self, prompt=""):
+    def input(self, prompt=""):
         try: return input(prompt)
-        except EOFError: self._exit()
-        except KeyboardInterrupt: self._exit()
+        except EOFError: self.exit()
+        except KeyboardInterrupt: self.exit()
     
-    def _getch(self, string=False):
+    def getch(self, string=False):
         char = None
         if string:
             string = ""
@@ -58,7 +66,7 @@ class Safe:
                     skip_char = True
                     continue
                 
-                if raw_char == 3 or raw_char == 17: self._exit() #Crtl + C or Crtl + Q
+                if raw_char == 3 or raw_char == 17: self.exit() #Crtl + C or Crtl + Q
                 if raw_char == 13: break
 
                 if raw_char == 8:
@@ -70,11 +78,11 @@ class Safe:
             return string
         
         else:
-            valid_getchs = range(49, 49 + self.num_items) #Number inputs = number + 49
+            validgetchs = range(49, 49 + self.num_items) #Number inputs = number + 49
 
-            while not char in valid_getchs:
+            while not char in validgetchs:
                 char = ord(getch())
-                if char == 3 or char == 17: self._exit() #Crtl + C or Crtl + Q
+                if char == 3 or char == 17: self.exit() #Crtl + C or Crtl + Q
 
             return char - 49
     
@@ -86,33 +94,31 @@ class Safe:
 3. Exit""")
 
         self.num_items = 3
-        char = self._getch()
+        char = self.getch()
         
-        if char == 0: self._login()
-        elif char == 1: self._create_account()
+        if char == 0: self.login()
+        elif char == 1: self.create_account()
 
-        self._exit()
+        self.exit()
 
-    def _login(self):
+    def login(self):
         cls()
-        attempts = 3
-        while attempts > 0:
-            attempts -= 1
-            email = self._input("Email address:\n")
+        email = self.input("Email address:\n")
         
         attempts = 3
         while attempts > 0:
             attempts -= 1
             print("Password:")
-            password = self._getch(True)
+            password = self.getch(True)
             
-    def _create_account(self):
+    def create_account(self, first_time=False):
         
         # Email > Begin
         cls()
+        if first_time: print("Welcome to PYssword-Safe.\nIt seems as if you have not run this program before, so I'll help you set up a new account.\nFirst, please enter your email address.\n")
         while True:
             print("Email address:")
-            email = re.match("^.+[@].+[\.].+$", self._input())
+            email = re.match("^.+[@].+[\.].+$", self.input())
             if email: break
             cls()
             print("Email must follow the form name@provider.extension")
@@ -123,14 +129,35 @@ class Safe:
         # Password > Begin
         cls()
         while True:
-            print("Password:")
-            password = self._getch(True)
-            password = re.match("^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8,56}$", password)
-            if password: break
-            cls()
-            print("Password must have at least one capital letter, one number, and one symbol.\nValid symbols are: !@#$&*\nPassword must also be between 8 and 56 characters (inclusive)")
+            while True:
+                print("Password:")
+                password = self.getch(True)
+                password = password_check.match(password)
+                if password: break
+                cls()
+                print("Password must have at least one capital letter, one number, and one symbol.\nValid symbols are: !@#$&*\nPassword must also be between 8 and 56 characters (inclusive).\n")
+            
+            password = password.group(0)
 
-        password = password.group(0)
+            cls()
+            attempts = 3
+            while attempts > 0:
+                attempts -= 1
+                print("Type your password again:")
+                pwd2 = self.getch(True)
+                pwd2 = password_check.match(pwd2)
+
+                if pwd2:
+                    pwd2 = pwd2.group(0)
+                    if password == pwd2: break
+
+                cls()
+                print("Passwords do not match.\n")
+
+            if password == pwd2: break
+
+            cls()
+            print("Too many incorrect attempts. Please retype your orginal password.\n")
         # Password > End
 
         # Username > Begin
@@ -138,21 +165,21 @@ class Safe:
         fallback = self.current_user.split("@")[0]
         
         print("Name: [{}]".format(fallback))
-        self.username = self._input()
+        self.username = self.input()
 
-        if self.username = "": self.username = fallback
+        if self.username == "": self.username = fallback
         # Username > End
 
-        
         salt = generate_salt()
         key = password.encode('utf-8') + salt.encode('utf-8')
         hashed_password = hashlib.sha512(key).hexdigest()
 
-        self._raw_data[self.current_user] = [self.username, hashed_password, salt, {}]
-        self.cur_data = self._raw_data[self.current_user][2]
+        self.raw_data[self.current_user] = [self.username, hashed_password, salt, {}]
+        self.cur_data = self.raw_data[self.current_user][3]
+        self.cur_data["Example account"] = "Example password"
 
-        self.cipher = Fernet(base64.urlsafe_b64encode(key[0:32]))
+        self.cipher = Fernet(base64.urlsafe_b64encode(key[0:32])) #Using the first 32 characters of the password + salt (as utf-8), converted to base 64
         #http://docs.python-guide.org/en/latest/scenarios/crypto/
-        self._exit()
+        self.exit()
 
 Safe()
