bf1d6fffe70ba6695b09b2d8bdb1fcded211273f
Tomais Williamson <williamson.tomais@gmail.com>
2016-09-07 09:58:43 +1200

    Patches for building
    Using pyinstaller

--- PYssword-Safe.py
+++ PYssword-Safe.py
@@ -11,6 +11,31 @@ from msvcrt import getch
 from cryptography.fernet import Fernet
 import sys, os, hashlib, json, re, random, base64
 
+def patch_crypto_be_discovery(): #From https://github.com/pyinstaller/pyinstaller/issues/2013
+
+    """
+    Monkey patches cryptography's backend detection.
+    Objective: support pyinstaller freezing.
+    """
+
+    from cryptography.hazmat import backends
+
+    try:
+        from cryptography.hazmat.backends.commoncrypto.backend import backend as be_cc
+    except ImportError:
+        be_cc = None
+
+    try:
+        from cryptography.hazmat.backends.openssl.backend import backend as be_ossl
+    except ImportError:
+        be_ossl = None
+
+    backends._available_backends_list = [
+        be for be in (be_cc, be_ossl) if be is not None
+    ]
+
+patch_crypto_be_discovery()
+
 #=== Planned file storage ===#
 #File contents: {"user@email.com": ("username", "hashed master password", "salt", "encrypted text")}
 #Encrypted text: {"Account": "password"}
